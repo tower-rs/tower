c06aa5452d130da948c0b9a3b587edc094b19045
Oliver Gould <ver@buoyant.io>
2018-01-26 14:23:11 -0800

    Test `Balance::poll_ready` (#42)
    Test `Balance::poll_ready` by creating a random number of services, each
    of which must be polled a random number of times before becoming ready.
    As the balancer is polled, the test ensures that it does not become
    ready prematurely and that services are promoted from not_ready to
    ready.
    
    `Balance::num_ready()` and `Balance::is_ready()` have been added to
    expose the number of ready services, as well as `Balance::num_not_ready()`
    to expose the number of pending services.

--- tower-balance/src/lib.rs
+++ tower-balance/src/lib.rs
@@ -106,6 +106,34 @@ where
         }
     }
 
+    /// Returns true iff there are ready services.
+    ///
+    /// This is not authoritative and is only useful after `poll_ready` has been called.
+    fn is_ready(&self) -> bool {
+        !self.ready.is_empty()
+    }
+
+    /// Returns true iff there are no ready services.
+    ///
+    /// This is not authoritative and is only useful after `poll_ready` has been called.
+    pub fn is_not_ready(&self) -> bool {
+        self.ready.is_empty()
+    }
+
+    /// Counts the number of services considered to be ready.
+    ///
+    /// This is not authoritative and is only useful after `poll_ready` has been called.
+    pub fn num_ready(&self) -> usize {
+        self.ready.len()
+    }
+
+    /// Counts the number of services not considered to be ready.
+    ///
+    /// This is not authoritative and is only useful after `poll_ready` has been called.
+    pub fn num_not_ready(&self) -> usize {
+        self.not_ready.len()
+    }
+
     /// Polls `discover` for updates, adding new items to `not_ready`.
     ///
     /// Removals may alter the order of either `ready` or `not_ready`.
@@ -281,5 +309,142 @@ impl<F: Future, E> Future for ResponseFuture<F, E> {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         self.0.poll().map_err(Error::Inner)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::future;
+    use quickcheck::*;
+    use tower_discover::Change;
+    use std::collections::VecDeque;
+
+    use super::*;
+
+    struct ReluctantDisco(VecDeque<Change<usize, ReluctantService>>);
+
+    struct ReluctantService {
+        polls_until_ready: usize,
+    }
+
+    impl Discover for ReluctantDisco {
+        type Key = usize;
+        type Request = ();
+        type Response = ();
+        type Error = ();
+        type Service = ReluctantService;
+        type DiscoverError = ();
+
+        fn poll(&mut self) -> Poll<Change<Self::Key, Self::Service>, Self::DiscoverError> {
+            let r = self.0.pop_front().map(Async::Ready).unwrap_or(Async::NotReady);
+            debug!("polling disco: {:?}", r.is_ready());
+            Ok(r)
+        }
+    }
+
+    impl Service for ReluctantService {
+        type Request = ();
+        type Response = ();
+        type Error = ();
+        type Future = future::FutureResult<(), ()>;
+
+        fn poll_ready(&mut self) -> Poll<(), ()> {
+            if self.polls_until_ready == 0 {
+                return Ok(Async::Ready(()));
+            }
+
+            self.polls_until_ready -= 1;
+            return Ok(Async::NotReady);
+        }
+
+        fn call(&mut self, _: ()) -> Self::Future {
+            future::ok(())
+        }
+    }
+
+    quickcheck! {
+        /// Creates a random number of services, each of which must be polled a random
+        /// number of times before becoming ready. As the balancer is polled, ensure that
+        /// it does not become ready prematurely and that services are promoted from
+        /// not_ready to ready.
+        fn poll_ready(service_tries: Vec<usize>) -> TestResult {
+            // Stores the number of pending services after each poll_ready call.
+            let mut pending_at = Vec::new();
+
+            let disco = {
+                let mut changes = VecDeque::new();
+                for (i, n) in service_tries.iter().map(|n| *n).enumerate() {
+                    for j in 0..n {
+                        if j == pending_at.len() {
+                            pending_at.push(1);
+                        } else {
+                            pending_at[j] += 1;
+                        }
+                    }
+
+                    let s = ReluctantService { polls_until_ready: n };
+                    changes.push_back(Change::Insert(i, s));
+                }
+                ReluctantDisco(changes)
+            };
+            pending_at.push(0);
+
+            let mut balancer = Balance::new(disco, choose::RoundRobin::default());
+
+            let services = service_tries.len();
+            let mut next_pos = 0;
+            for pending in pending_at.iter().map(|p| *p) {
+                assert!(pending <= services);
+                let ready = services - pending;
+
+                match balancer.poll_ready() {
+                    Err(_) => return TestResult::error("poll_ready failed"),
+                    Ok(p) => {
+                        if p.is_ready() != (ready > 0) {
+                            return TestResult::failed();
+                        }
+                    }
+                }
+
+                if balancer.num_ready() != ready {
+                    return TestResult::failed();
+                }
+
+                if balancer.num_not_ready() != pending {
+                    return TestResult::failed();
+                }
+
+                if balancer.is_ready() != (ready > 0) {
+                    return TestResult::failed();
+                }
+                if balancer.is_not_ready() != (ready == 0) {
+                    return TestResult::failed();
+                }
+
+                if balancer.dispatched_ready_index.is_some() {
+                    return TestResult::failed();
+                }
+
+                if ready == 0 {
+                    if balancer.chosen_ready_index.is_some() {
+                        return TestResult::failed();
+                    }
+                } else {
+                    // Check that the round-robin chooser is doing its thing:
+                    match balancer.chosen_ready_index {
+                        None => return TestResult::failed(),
+                        Some(idx) => {
+                            if idx != next_pos  {
+                                return TestResult::failed();
+                            }
+                        }
+                    }
+
+                    next_pos = (next_pos + 1) % ready;
+                }
+            }
+
+            TestResult::passed()
+        }
     }
 }
