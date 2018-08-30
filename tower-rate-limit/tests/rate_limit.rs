89666da31948493d9d83b7947154d40704a78b10
Eliza Weisman <eliza@buoyant.io>
2018-08-30 16:50:45 -0700

    Test review feedback

--- tower-rate-limit/tests/rate_limit.rs
+++ tower-rate-limit/tests/rate_limit.rs
@@ -5,7 +5,7 @@ extern crate tower_service;
 extern crate tokio_timer;
 extern crate tokio;
 
-use futures::{prelude::*, future};
+use futures::future;
 use tower_rate_limit::*;
 use tower_service::*;
 
@@ -13,49 +13,48 @@ use std::time::{Duration, Instant};
 
 #[test]
 fn reaching_capacity() {
-    let mut rt = tokio::runtime::current_thread::Runtime::new().unwrap();
-    rt.block_on(future::lazy(|| {
-        let (mut service, mut handle) =
-            new_service(Rate::new(1, from_millis(100)));
+    let mut rt = tokio::runtime::current_thread::Runtime::new()
+        .unwrap();
+    let (mut service, mut handle) =
+        new_service(Rate::new(1, from_millis(100)));
 
-        let response = service.call("hello");
+    let response = service.call("hello");
 
-        let request = handle.next_request().unwrap();
-        assert_eq!(*request, "hello");
-        request.respond("world");
+    let request = handle.next_request().unwrap();
+    assert_eq!(*request, "hello");
+    request.respond("world");
 
-        response.then(move |response| Ok((response, service, handle)))
-    }).and_then(|(response, mut service, mut handle)| {
-        assert_eq!(response.unwrap(), "world");
+    let response = rt.block_on(response);
+    assert_eq!(response.unwrap(), "world");
 
-        // Sending another request is rejected
-        let response = service.call("no");
-        assert!(handle.poll_request().unwrap().is_not_ready());
+    // Sending another request is rejected
+    let response = service.call("no");
 
-        response.then(move |response| Ok((response, service, handle)))
-    }).and_then(|(response, mut service, handle)| {
-        assert!(response.is_err());
-        assert!(service.poll_ready().unwrap().is_not_ready());
+    let poll_request = rt.block_on(future::lazy(|| handle.poll_request()));
+    assert!(poll_request.unwrap().is_not_ready());
 
-        tokio_timer::Delay::new(Instant::now() + Duration::from_millis(100))
-            .then(move |_| Ok((service, handle)))
-    }).and_then(|(mut service, mut handle)| {
+    let response = rt.block_on(response);
+    assert!(response.is_err());
 
-        assert!(service.poll_ready().unwrap().is_ready());
+    let poll_request = rt.block_on(future::lazy(|| handle.poll_request()));
+    assert!(poll_request.unwrap().is_not_ready());
 
-        // Send a second request
-        let response = service.call("two");
+    // Unlike `thread::sleep`, this advances the timer.
+    rt.block_on(tokio_timer::Delay::new(Instant::now() + Duration::from_millis(100)))
+        .unwrap();
 
-        let request = handle.next_request().unwrap();
-        assert_eq!(*request, "two");
-        request.respond("done");
+    let poll_ready = rt.block_on(future::lazy(|| service.poll_ready()));
+    assert!(poll_ready.unwrap().is_ready());
 
-        response
-    }).then(|response| {
-        assert_eq!(response.unwrap(), "done");
+    // Send a second request
+    let response = service.call("two");
 
-        Ok::<(), ()>(())
-    })).unwrap();
+    let request = handle.next_request().unwrap();
+    assert_eq!(*request, "two");
+    request.respond("done");
+
+    let response = rt.block_on(response);
+    assert_eq!(response.unwrap(), "done");
 }
 
 type Mock = tower_mock::Mock<&'static str, &'static str, ()>;
