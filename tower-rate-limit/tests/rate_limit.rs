b5af39436671ee6fbc8365b41ae5b537b1228875
Eliza Weisman <eliza@buoyant.io>
2019-01-12 12:08:31 -0800

    Update tower-rate-limit to work with new tokio_timer (#100)

--- tower-rate-limit/tests/rate_limit.rs
+++ tower-rate-limit/tests/rate_limit.rs
@@ -3,16 +3,18 @@ extern crate tower_mock;
 extern crate tower_rate_limit;
 extern crate tower_service;
 extern crate tokio_timer;
+extern crate tokio;
 
-use futures::prelude::*;
+use futures::future;
 use tower_rate_limit::*;
 use tower_service::*;
 
-use std::time::Duration;
-use std::thread;
+use std::time::{Duration, Instant};
 
 #[test]
 fn reaching_capacity() {
+    let mut rt = tokio::runtime::current_thread::Runtime::new()
+        .unwrap();
     let (mut service, mut handle) =
         new_service(Rate::new(1, from_millis(100)));
 
@@ -22,25 +24,27 @@ fn reaching_capacity() {
     assert_eq!(*request, "hello");
     request.respond("world");
 
-    assert_eq!(response.wait().unwrap(), "world");
+    let response = rt.block_on(response);
+    assert_eq!(response.unwrap(), "world");
 
     // Sending another request is rejected
     let response = service.call("no");
-    with_task(|| {
-        assert!(handle.poll_request().unwrap().is_not_ready());
-    });
 
-    assert!(response.wait().is_err());
+    let poll_request = rt.block_on(future::lazy(|| handle.poll_request()));
+    assert!(poll_request.unwrap().is_not_ready());
 
-    with_task(|| {
-        assert!(service.poll_ready().unwrap().is_not_ready());
-    });
+    let response = rt.block_on(response);
+    assert!(response.is_err());
 
-    thread::sleep(Duration::from_millis(100));
+    let poll_request = rt.block_on(future::lazy(|| handle.poll_request()));
+    assert!(poll_request.unwrap().is_not_ready());
 
-    with_task(|| {
-        assert!(service.poll_ready().unwrap().is_ready());
-    });
+    // Unlike `thread::sleep`, this advances the timer.
+    rt.block_on(tokio_timer::Delay::new(Instant::now() + Duration::from_millis(100)))
+        .unwrap();
+
+    let poll_ready = rt.block_on(future::lazy(|| service.poll_ready()));
+    assert!(poll_ready.unwrap().is_ready());
 
     // Send a second request
     let response = service.call("two");
@@ -49,25 +53,17 @@ fn reaching_capacity() {
     assert_eq!(*request, "two");
     request.respond("done");
 
-    assert_eq!(response.wait().unwrap(), "done");
+    let response = rt.block_on(response);
+    assert_eq!(response.unwrap(), "done");
 }
 
 type Mock = tower_mock::Mock<&'static str, &'static str, ()>;
 type Handle = tower_mock::Handle<&'static str, &'static str, ()>;
 
 fn new_service(rate: Rate) -> (RateLimit<Mock>, Handle) {
-    let timer = tokio_timer::wheel()
-        .tick_duration(Duration::from_millis(1))
-        .build();
-
     let (service, handle) = Mock::new();
-    let service = RateLimit::new(service, rate, timer);
+    let service = RateLimit::new(service, rate);
     (service, handle)
-}
-
-fn with_task<F: FnOnce() -> U, U>(f: F) -> U {
-    use futures::future::{Future, lazy};
-    lazy(|| Ok::<_, ()>(f())).wait().unwrap()
 }
 
 fn from_millis(n: u64) -> Duration {
