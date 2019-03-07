1e38ee6e1f8f7a99e6a46a946d89eb9e6130b851
Carl Lerche <me@carllerche.com>
2019-03-07 13:53:21 -0800

    Fix in flight limit (#184)
    * Move `InFlightLimitLayer` into `layer` mod
    
    * Remove error type

---
+++ tower-in-flight-limit/src/layer.rs
@@ -0,0 +1,29 @@
+use tower_layer::Layer;
+use tower_service::Service;
+use {Error, InFlightLimit};
+
+#[derive(Debug, Clone)]
+pub struct InFlightLimitLayer {
+    max: usize,
+}
+
+impl InFlightLimitLayer {
+    pub fn new(max: usize) -> Self {
+        InFlightLimitLayer { max }
+    }
+}
+
+impl<S, Request> Layer<S, Request> for InFlightLimitLayer
+where
+    S: Service<Request>,
+    S::Error: Into<Error>,
+{
+    type Response = S::Response;
+    type Error = Error;
+    type LayerError = ();
+    type Service = InFlightLimit<S>;
+
+    fn layer(&self, service: S) -> Result<Self::Service, Self::LayerError> {
+        Ok(InFlightLimit::new(service, self.max))
+    }
+}
