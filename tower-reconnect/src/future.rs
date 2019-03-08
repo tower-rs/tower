e0d6d5b2f548d7f2cf9676d601708f5db412709b
Carl Lerche <me@carllerche.com>
2019-03-08 08:46:12 -0800

    Refresh Reconnect (#185)

---
+++ tower-reconnect/src/future.rs
@@ -0,0 +1,25 @@
+use futures::{Future, Poll};
+use Error;
+
+pub struct ResponseFuture<F> {
+    inner: F,
+}
+
+impl<F> ResponseFuture<F> {
+    pub(crate) fn new(inner: F) -> Self {
+        ResponseFuture { inner }
+    }
+}
+
+impl<F> Future for ResponseFuture<F>
+where
+    F: Future,
+    F::Error: Into<Error>,
+{
+    type Item = F::Item;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.inner.poll().map_err(Into::into)
+    }
+}
