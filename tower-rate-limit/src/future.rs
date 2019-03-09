720d31c65ff2ec9c2531dc7f5c46bc0456847295
Carl Lerche <me@carllerche.com>
2019-03-08 22:44:48 -0800

    rate-limit: Refresh layout (#189)
    - Switch to `Box<Error>`
    - Break up lib.rs into multiple files.
    - Use `tokio::clock::now` instead of `Instant::now`.

---
+++ tower-rate-limit/src/future.rs
@@ -0,0 +1,26 @@
+use crate::Error;
+use futures::{Future, Poll};
+
+#[derive(Debug)]
+pub struct ResponseFuture<T> {
+    inner: T,
+}
+
+impl<T> ResponseFuture<T> {
+    pub(crate) fn new(inner: T) -> ResponseFuture<T> {
+        ResponseFuture { inner }
+    }
+}
+
+impl<T> Future for ResponseFuture<T>
+where
+    T: Future,
+    Error: From<T::Error>,
+{
+    type Item = T::Item;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.inner.poll().map_err(Into::into)
+    }
+}
