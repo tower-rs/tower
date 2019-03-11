50fc5e8e630cbb23bce283d773a4e8b6523d14bc
Carl Lerche <me@carllerche.com>
2019-03-11 16:10:51 -0700

    Reorganize tower-timeout (#191)
    - Change error bound: `Error: From<S::Error>` (for `?` operator)
    - Split types into dedicated files
    - Move `ResponseFuture` into public `future` mod.

---
+++ tower-timeout/src/layer.rs
@@ -0,0 +1,32 @@
+use crate::{Error, Timeout};
+use std::time::Duration;
+use tower_layer::Layer;
+use tower_service::Service;
+
+/// Applies a timeout to requests via the supplied inner service.
+#[derive(Debug)]
+pub struct TimeoutLayer {
+    timeout: Duration,
+}
+
+impl TimeoutLayer {
+    /// Create a timeout from a duration
+    pub fn new(timeout: Duration) -> Self {
+        TimeoutLayer { timeout }
+    }
+}
+
+impl<S, Request> Layer<S, Request> for TimeoutLayer
+where
+    S: Service<Request>,
+    Error: From<S::Error>,
+{
+    type Response = S::Response;
+    type Error = Error;
+    type LayerError = ();
+    type Service = Timeout<S>;
+
+    fn layer(&self, service: S) -> Result<Self::Service, Self::LayerError> {
+        Ok(Timeout::new(service, self.timeout))
+    }
+}
