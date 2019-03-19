e9ab84f0e1500c7727b445542b8a36b0b24db4be
Sean McArthur <sean@seanmonstar.com>
2019-03-19 13:07:51 -0700

    add poll_ready to MakeConnection

--- tower-service-util/src/make_connection.rs
+++ tower-service-util/src/make_connection.rs
@@ -1,5 +1,5 @@
 use crate::sealed::Sealed;
-use futures::Future;
+use futures::{Future, Poll};
 use tokio_io::{AsyncRead, AsyncWrite};
 use tower_service::Service;
 
@@ -10,13 +10,16 @@ use tower_service::Service;
 /// based connection or using some other method to authenticate the connection.
 pub trait MakeConnection<Request>: Sealed<(Request,)> {
     /// The transport provided by this service
-    type Response: AsyncRead + AsyncWrite;
+    type Connection: AsyncRead + AsyncWrite;
 
     /// Errors produced by the connecting service
     type Error;
 
     /// The future that eventually produces the transport
-    type Future: Future<Item = Self::Response, Error = Self::Error>;
+    type Future: Future<Item = Self::Connection, Error = Self::Error>;
+
+    /// Returns `Ready` when it is able to make more connections.
+    fn poll_ready(&mut self) -> Poll<(), Self::Error>;
 
     /// Connect and return a transport asynchronously
     fn make_connection(&mut self, target: Request) -> Self::Future;
@@ -29,9 +32,13 @@ where
     C: Service<Request>,
     C::Response: AsyncRead + AsyncWrite,
 {
-    type Response = C::Response;
+    type Connection = C::Response;
     type Error = C::Error;
     type Future = C::Future;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        Service::poll_ready(self)
+    }
 
     fn make_connection(&mut self, target: Request) -> Self::Future {
         Service::call(self, target)
