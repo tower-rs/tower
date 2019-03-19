ac1581f064531c69269710652cfdd2c154ce5f60
David Barsky <dbarsky@amazon.com>
2019-03-19 10:08:17 -0400

    Remove trait + lazy_static

--- tower-blocking-service/src/lib.rs
+++ tower-blocking-service/src/lib.rs
@@ -1,47 +1,26 @@
-use lazy_static::lazy_static;
-use std::sync::{Arc, Mutex};
-use tokio::runtime::Runtime;
+use tokio::runtime::current_thread::Runtime;
 use tower_service::Service;
 use tower_service_util::BoxService;
 
-lazy_static! {
-    static ref RUNTIME: Arc<Mutex<Runtime>> = Arc::new(Mutex::new(
-        Runtime::new().expect("Runtime could not be created")
-    ));
+pub struct BlockingSvc<Request, Response, Err> {
+    inner: BoxService<Request, Response, Err>,
+    rt: Runtime,
 }
 
-trait BlockingService<Request> {
-    type Response;
-    type Error;
-
-    fn call(&mut self, req: Request) -> Result<Self::Response, Self::Error>;
-}
-
-struct BlockingSvc<T, U, E> {
-    inner: BoxService<T, U, E>,
-}
-
-impl<T, U, E> BlockingSvc<T, U, E> {
-    fn new<S>(inner: S) -> Self
+impl<Request, Response, Err> BlockingSvc<Request, Response, Err> {
+    pub fn new<S>(inner: S) -> Self
     where
-        S: Service<T, Response = U, Error = E> + Send + 'static,
+        S: Service<Request, Response = Response, Error = Err> + Send + 'static,
         S::Future: Send + 'static,
     {
         Self {
             inner: BoxService::new(inner),
+            rt: Runtime::new().unwrap(),
         }
     }
-}
 
-impl<Request, U, E> BlockingService<Request> for BlockingSvc<Request, U, E>
-where
-    E: Send + 'static,
-    U: Send + 'static,
-{
-    type Response = U;
-    type Error = E;
-
-    fn call(&mut self, req: Request) -> Result<Self::Response, Self::Error> {
-        RUNTIME.lock().unwrap().block_on(self.inner.call(req))
+    pub fn call(&mut self, req: Request) -> Result<Response, Err> {
+        self.inner.poll_ready()?;
+        self.rt.block_on(self.inner.call(req))
     }
 }
