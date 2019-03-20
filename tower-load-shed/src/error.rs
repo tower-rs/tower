ee15ebb6f97d52476a8e1037cee69e43e3fc509c
Sean McArthur <sean@seanmonstar.com>
2019-03-19 17:38:55 -0700

    Introduce tower-load-shed
    Provides a middleware that immediately fails any request if the
    underlying service isn't ready yet. This is useful when used in
    conjunction with `InFlightLimit` or others, so that requests are
    rejected immediately, instead of keeping track of extra pending
    requests.

---
+++ tower-load-shed/src/error.rs
@@ -0,0 +1,48 @@
+//! Error types
+
+use std::fmt;
+
+pub(crate) type Error = Box<dyn std::error::Error + Send + Sync>;
+pub(crate) use self::never::Never;
+
+/// An error returned by `Overload` when the underlying service
+/// is not ready to handle any requests at the time of being
+/// called.
+pub struct Overloaded {
+    _p: (),
+}
+
+impl Overloaded {
+    pub(crate) fn new() -> Self {
+        Overloaded { _p: () }
+    }
+}
+
+impl fmt::Debug for Overloaded {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Overloaded")
+    }
+}
+
+impl fmt::Display for Overloaded {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("service overloaded")
+    }
+}
+
+impl std::error::Error for Overloaded {}
+
+pub(crate) mod never {
+    use std::{error, fmt};
+
+    #[derive(Debug)]
+    pub enum Never {}
+
+    impl fmt::Display for Never {
+        fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
+            match *self {}
+        }
+    }
+
+    impl error::Error for Never {}
+}
