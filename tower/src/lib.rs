6edb4dbdabd7368b4fe73b451cd139d1cba460e4
Lucio Franco <luciofranco14@gmail.com>
2019-03-26 14:15:04 -0400

    Add all layers and MakeService utils to tower

--- tower/src/lib.rs
+++ tower/src/lib.rs
@@ -1,14 +1,37 @@
 //! Various utility types and functions that are generally with Tower.
 
-pub mod builder;
 #[macro_use]
 extern crate futures;
-#[cfg(test)]
-extern crate tokio_mock_task;
+
 extern crate tower_layer;
 extern crate tower_service;
 extern crate tower_service_util;
 
+#[cfg(feature = "layers")]
+extern crate tower_buffer;
+#[cfg(feature = "layers")]
+extern crate tower_filter;
+#[cfg(feature = "layers")]
+extern crate tower_in_flight_limit;
+#[cfg(feature = "layers")]
+extern crate tower_load_shed;
+#[cfg(feature = "layers")]
+extern crate tower_rate_limit;
+#[cfg(feature = "layers")]
+extern crate tower_retry;
+
+#[cfg(feature = "make_service")]
+extern crate tower_balance;
+#[cfg(feature = "make_service")]
+extern crate tower_discover;
+#[cfg(feature = "make_service")]
+extern crate tower_reconnect;
+
+pub mod builder;
+#[cfg(feature = "layers")]
+pub mod layer;
+#[cfg(feature = "make_service")]
+pub mod make_service;
 pub mod util;
 
 pub use builder::ServiceBuilder;
