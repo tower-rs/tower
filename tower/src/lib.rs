99833473928e82aab017085a5a2d75c9ce6127e4
Carl Lerche <me@carllerche.com>
2019-04-02 16:22:23 -0700

    re-export layers from `tower` crate. (#222)

--- tower/src/lib.rs
+++ tower/src/lib.rs
@@ -1,3 +1,7 @@
+// Allows refining features in the future without breaking backwards
+// compatibility
+#![cfg(feature = "full")]
+
 //! Various utility types and functions that are generally with Tower.
 
 #[macro_use]
@@ -7,33 +11,19 @@ extern crate tower_layer;
 extern crate tower_service;
 extern crate tower_util;
 
-#[cfg(feature = "layers")]
-extern crate tower_buffer;
-#[cfg(feature = "layers")]
-extern crate tower_filter;
-#[cfg(feature = "layers")]
-extern crate tower_in_flight_limit;
-#[cfg(feature = "layers")]
-extern crate tower_load_shed;
-#[cfg(feature = "layers")]
-extern crate tower_rate_limit;
-#[cfg(feature = "layers")]
-extern crate tower_retry;
-#[cfg(feature = "layers")]
-extern crate tower_timeout;
-
-#[cfg(feature = "make_service")]
-extern crate tower_balance;
-#[cfg(feature = "make_service")]
-extern crate tower_discover;
-#[cfg(feature = "make_service")]
-extern crate tower_reconnect;
+pub extern crate tower_balance as balance;
+pub extern crate tower_buffer as buffer;
+pub extern crate tower_discover as discover;
+pub extern crate tower_filter as filter;
+pub extern crate tower_in_flight_limit as in_flight_limit;
+pub extern crate tower_load_shed as load_shed;
+pub extern crate tower_rate_limit as rate_limit;
+pub extern crate tower_reconnect as reconnect;
+pub extern crate tower_retry as retry;
+pub extern crate tower_timeout as timeout;
 
 pub mod builder;
-#[cfg(feature = "layers")]
 pub mod layer;
-#[cfg(feature = "make_service")]
-pub mod make_service;
 pub mod util;
 
 pub use builder::ServiceBuilder;
