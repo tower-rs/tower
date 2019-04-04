4ed96725d9777da83a885a2339d7b8eebc300f89
Oliver Gould <ver@buoyant.io>
2019-04-04 01:29:44 +0000

    Set a PartialOrd constraint on Load::Metric

--- tower-balance/src/load/mod.rs
+++ tower-balance/src/load/mod.rs
@@ -14,7 +14,7 @@ pub use self::pending_requests::{PendingRequests, WithPendingRequests};
 /// Implementors should choose load values so that lesser-loaded instances return lesser
 /// values than higher-load instances.
 pub trait Load {
-    type Metric;
+    type Metric: PartialOrd;
 
     fn load(&self) -> Self::Metric;
 }
