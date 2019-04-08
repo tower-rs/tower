5691177ad3eaa3f45203296f2fcb172a8fcaa65f
Carl Lerche <me@carllerche.com>
2019-04-08 14:35:13 -0700

    Remove `LayerExt`

--- tower/src/layer.rs
+++ tower/src/layer.rs
@@ -6,21 +6,3 @@ pub mod util {
     pub use tower_util::layer::Stack;
     pub use tower_util::layer::Identity;
 }
-
-/// An extension trait for `Layer`'s that provides a variety of convenient
-/// adapters.
-pub trait LayerExt<S, Request>: Layer<S, Request> {
-    /// Return a new `Layer` instance that applies both `self` and
-    /// `middleware` to services being wrapped.
-    ///
-    /// This defines a middleware stack.
-    fn chain<T>(self, middleware: T) -> util::Stack<Self, T>
-    where
-        T: Layer<Self::Service, Request>,
-        Self: Sized,
-    {
-        util::Stack::new(self, middleware)
-    }
-}
-
-impl<T, S, Request> LayerExt<S, Request> for T where T: Layer<S, Request> {}
