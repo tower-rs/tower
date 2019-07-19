72219ce862e468a57a6f83d1f018d5d09257711f
Lucio Franco <luciofranco14@gmail.com>
2019-07-19 14:21:07 -0400

    Prep buffer and tower release (#305)
    * Prep buffer 0.1.1 release
    
    * Prep release for tower 0.1.1

--- tower/src/lib.rs
+++ tower/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/tower/0.1.1")]
 // Allows refining features in the future without breaking backwards
 // compatibility
 #![cfg(feature = "full")]
