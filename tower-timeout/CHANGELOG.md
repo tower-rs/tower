f259ebc139f85efdaaa2147726983e47c2c7421d
Lucio Franco <luciofranco14@gmail.com>
2019-07-30 14:54:39 -0400

    Prep tower-timeout 0.1.1 release

--- tower-timeout/CHANGELOG.md
+++ tower-timeout/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1 (July 30th, 2019)
+
+- Add `Elapsed::new`
+
 # 0.1.0 (April 26, 2019)
 
 - Initial release
--- tower-timeout/src/lib.rs
+++ tower-timeout/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-timeout/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/tower-timeout/0.1.1")]
 #![deny(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 #![allow(elided_lifetimes_in_paths)]
 #![cfg_attr(test, deny(warnings))]
