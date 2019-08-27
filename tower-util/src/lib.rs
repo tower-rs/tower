c9d0ffb58d81d9eef386e37e99785799185dc7e8
Lucio Franco <luciofranco14@gmail.com>
2019-08-26 20:51:08 -0400

    Remove Makers from tower-util

--- tower-util/src/lib.rs
+++ tower-util/src/lib.rs
@@ -4,43 +4,37 @@
 
 //! Various utility types and functions that are generally with Tower.
 
-// mod boxed;
-// mod call_all;
-// mod either;
-// pub mod layer;
-#[cfg(feature = "io")]
-mod make_connection;
-mod make_service;
-// mod oneshot;
-// mod optional;
-// mod ready;
+mod boxed;
+mod call_all;
+mod either;
+pub mod layer;
+mod oneshot;
+mod optional;
+mod ready;
 mod sealed;
-// mod service_fn;
+mod service_fn;
 
-#[cfg(feature = "io")]
-pub use crate::make_connection::MakeConnection;
-pub use crate::make_service::MakeService;
-// pub use crate::{
-//     boxed::{BoxService, UnsyncBoxService},
-//     call_all::{CallAll, CallAllUnordered},
-//     either::Either,
-//     make_service::MakeService,
-//     oneshot::Oneshot,
-//     optional::Optional,
-//     ready::Ready,
-//     service_fn::{service_fn, ServiceFn},
-// };
+pub use crate::{
+    boxed::{BoxService, UnsyncBoxService},
+    call_all::{CallAll, CallAllUnordered},
+    either::Either,
+    make_service::MakeService,
+    oneshot::Oneshot,
+    optional::Optional,
+    ready::Ready,
+    service_fn::{service_fn, ServiceFn},
+};
 
-// pub mod error {
-//     //! Error types
+pub mod error {
+    //! Error types
 
-//     pub use crate::optional::error as optional;
-// }
+    pub use crate::optional::error as optional;
+}
 
-// pub mod future {
-//     //! Future types
+pub mod future {
+    //! Future types
 
-//     #[cfg(feature = "either")]
-//     pub use crate::either::future as either;
-//     pub use crate::optional::future as optional;
-// }
+    #[cfg(feature = "either")]
+    pub use crate::either::future as either;
+    pub use crate::optional::future as optional;
+}
