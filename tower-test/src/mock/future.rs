17816b196a43b7ee4dcde8394b8f4fcd526c6039
John Doneth <doneth7@gmail.com>
2019-09-01 14:50:54 -0400

    use pin_utils::unsafe_pinned

--- tower-test/src/mock/future.rs
+++ tower-test/src/mock/future.rs
@@ -18,6 +18,9 @@ pub struct ResponseFuture<T> {
 type Rx<T> = oneshot::Receiver<Result<T, Error>>;
 
 impl<T> ResponseFuture<T> {
+
+    pin_utils::unsafe_pinned!(rx: Option<Rx<T>>);
+
     pub(crate) fn new(rx: Rx<T>) -> ResponseFuture<T> {
         ResponseFuture { rx: Some(rx) }
     }
@@ -30,13 +33,13 @@ impl<T> ResponseFuture<T> {
 impl<T> Future for ResponseFuture<T> {
     type Output = Result<T, Error>;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        match self.rx {
-            Some(ref mut rx) => match Box::pin(rx).as_mut().poll(cx) {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        match self.rx().as_pin_mut() {
+            Some(rx) => match rx.poll(cx) {
                 Poll::Ready(Ok(Ok(v))) => Poll::Ready(Ok(v)),
                 Poll::Ready(Ok(Err(e))) => Poll::Ready(Err(e)),
-                Poll::Pending => Poll::Pending,
                 Poll::Ready(Err(_)) => Poll::Ready(Err(error::Closed::new().into())),
+                Poll::Pending => Poll::Pending,
             },
             None => Poll::Ready(Err(error::Closed::new().into())),
         }
