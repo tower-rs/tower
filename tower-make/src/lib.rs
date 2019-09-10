9691d0d379a9964da59082b4d17379ae99a3a7b0
Jon Gjengset <jon@thesquareplanet.com>
2019-09-10 11:48:01 -0400

    Update tower-reconnect to std::future (#333)
    This bumps tower-reconnect to 0.3.0-alpha.1
    
    It also makes the tower-make version consistent

--- tower-make/src/lib.rs
+++ tower-make/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-make/0.1.0-alpha.2")]
+#![doc(html_root_url = "https://docs.rs/tower-make/0.3.0-alpha.1")]
 #![deny(rust_2018_idioms)]
 
 //! Trait aliases for Services that produce specific types of Responses.
