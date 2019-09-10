06a089f5eeca32a5c22fd9c905e6b7803996da36
Jon Gjengset <jon@thesquareplanet.com>
2019-09-09 23:57:04 -0400

    Simplify test that doesn't need runtime

--- tower-util/tests/service_fn.rs
+++ tower-util/tests/service_fn.rs
@@ -1,10 +1,11 @@
+use futures_util::future::ready;
+use tokio_test::block_on;
 use tower_service::Service;
 use tower_util::service_fn;
-use futures_util::future::ready;
 
-#[tokio::test]
-async fn simple() {
+#[test]
+fn simple() {
     let mut add_one = service_fn(|req| ready(Ok::<_, ()>(req + 1)));
-    let answer = add_one.call(1).await.unwrap();
+    let answer = block_on(add_one.call(1)).unwrap();
     assert_eq!(answer, 2);
 }
