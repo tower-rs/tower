2693ee01504cda6fc316a00c1e94aa3803f65715
Jon Gjengset <jon@thesquareplanet.com>
2019-09-10 00:33:39 -0400

    This bumps tower-filter to 0.3.0-alpha.1

--- tower-filter/tests/filter.rs
+++ tower-filter/tests/filter.rs
@@ -1,15 +1,17 @@
-use futures::*;
-use std::thread;
+use futures_util::{future::poll_fn, pin_mut};
+use std::{future::Future, thread};
+use tokio_test::{assert_ready, assert_ready_err, task};
 use tower_filter::{error::Error, Filter};
 use tower_service::Service;
 use tower_test::{assert_request_eq, mock};
 
-#[test]
-fn passthrough_sync() {
-    let (mut service, mut handle) = new_service(|_| Ok(()));
+#[tokio::test]
+async fn passthrough_sync() {
+    let (mut service, handle) = new_service(|_| async { Ok(()) });
 
     let th = thread::spawn(move || {
         // Receive the requests and respond
+        pin_mut!(handle);
         for i in 0..10 {
             assert_request_eq!(handle, format!("ping-{}", i)).send_response(format!("pong-{}", i));
         }
@@ -17,29 +19,32 @@ fn passthrough_sync() {
 
     let mut responses = vec![];
 
-    for i in 0..10 {
+    for i in 0usize..10 {
         let request = format!("ping-{}", i);
-        assert!(service.poll_ready().unwrap().is_ready());
-        let exchange = service.call(request).and_then(move |response| {
+        poll_fn(|cx| service.poll_ready(cx)).await.unwrap();
+        let exchange = service.call(request);
+        let exchange = async move {
+            let response = exchange.await.unwrap();
             let expect = format!("pong-{}", i);
             assert_eq!(response.as_str(), expect.as_str());
-
-            Ok(())
-        });
+        };
 
         responses.push(exchange);
     }
 
-    future::join_all(responses).wait().unwrap();
+    futures_util::future::join_all(responses).await;
     th.join().unwrap();
 }
 
 #[test]
 fn rejected_sync() {
-    let (mut service, _handle) = new_service(|_| Err(Error::rejected()));
+    task::mock(|cx| {
+        let (mut service, _handle) = new_service(|_| async { Err(Error::rejected()) });
 
-    let response = service.call("hello".into()).wait();
-    assert!(response.is_err());
+        let fut = service.call("hello".into());
+        pin_mut!(fut);
+        assert_ready_err!(fut.poll(cx));
+    });
 }
 
 type Mock = mock::Mock<String, String>;
@@ -48,7 +53,7 @@ type Handle = mock::Handle<String, String>;
 fn new_service<F, U>(f: F) -> (Filter<Mock, F>, Handle)
 where
     F: Fn(&String) -> U,
-    U: IntoFuture<Item = (), Error = Error>,
+    U: Future<Output = Result<(), Error>>,
 {
     let (service, handle) = mock::pair();
     let service = Filter::new(service, f);
