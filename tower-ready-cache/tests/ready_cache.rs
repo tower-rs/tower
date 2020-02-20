ae34c9b4a12208daa71b9d8b4ca95d09d2d0c46d
Jon Gjengset <jon@thesquareplanet.com>
2020-02-20 16:03:09 -0500

    Add more tower-ready-cache tests

--- tower-ready-cache/tests/ready_cache.rs
+++ tower-ready-cache/tests/ready_cache.rs
@@ -69,3 +69,105 @@ fn poll_ready_promotes_inner() {
     assert_eq!(cache.pending_len(), 0);
     assert_eq!(cache.len(), 2);
 }
+
+#[test]
+fn evict_ready_then_error() {
+    let mut task = task::spawn(());
+    let mut cache = ReadyCache::<usize, Mock, Req>::default();
+
+    let (service, mut handle) = mock::pair::<Req, Req>();
+    handle.allow(0);
+    cache.push(0, service);
+
+    assert_pending!(task.enter(|cx, _| cache.poll_pending(cx)));
+
+    handle.allow(1);
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+
+    handle.send_error("doom");
+    assert!(cache.evict(&0));
+
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+}
+
+#[test]
+fn evict_pending_then_error() {
+    let mut task = task::spawn(());
+    let mut cache = ReadyCache::<usize, Mock, Req>::default();
+
+    let (service, mut handle) = mock::pair::<Req, Req>();
+    handle.allow(0);
+    cache.push(0, service);
+
+    assert_pending!(task.enter(|cx, _| cache.poll_pending(cx)));
+
+    handle.send_error("doom");
+    assert!(cache.evict(&0));
+
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+}
+
+#[test]
+fn push_then_evict() {
+    let mut task = task::spawn(());
+    let mut cache = ReadyCache::<usize, Mock, Req>::default();
+
+    let (service, mut handle) = mock::pair::<Req, Req>();
+    handle.allow(0);
+    cache.push(0, service);
+    handle.send_error("doom");
+    assert!(cache.evict(&0));
+
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+}
+
+#[test]
+fn error_after_promote() {
+    let mut task = task::spawn(());
+    let mut cache = ReadyCache::<usize, Mock, Req>::default();
+
+    let (service, mut handle) = mock::pair::<Req, Req>();
+    handle.allow(0);
+    cache.push(0, service);
+
+    assert_pending!(task.enter(|cx, _| cache.poll_pending(cx)));
+
+    handle.allow(1);
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+
+    handle.send_error("doom");
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+}
+
+#[test]
+fn duplicate_key_by_index() {
+    let mut task = task::spawn(());
+    let mut cache = ReadyCache::<usize, Mock, Req>::default();
+
+    let (service0, mut handle0) = mock::pair::<Req, Req>();
+    handle0.allow(1);
+    cache.push(0, service0);
+
+    let (service1, mut handle1) = mock::pair::<Req, Req>();
+    handle1.allow(1);
+    // this push should replace the old service (service0)
+    cache.push(0, service1);
+
+    // this evict should evict service1
+    cache.evict(&0);
+
+    // poll_pending should complete (there are no remaining pending services)
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+    // but service 0 should not be ready (1 replaced, 1 evicted)
+    assert!(!task.enter(|cx, _| cache.check_ready(cx, &0)).unwrap());
+
+    let (service2, mut handle2) = mock::pair::<Req, Req>();
+    handle2.allow(1);
+    // this push should ensure replace the evicted service1
+    cache.push(0, service2);
+
+    // there should be no more pending
+    assert_ready!(task.enter(|cx, _| cache.poll_pending(cx))).unwrap();
+    // _and_ service 0 should now be callable
+    assert!(task.enter(|cx, _| cache.check_ready(cx, &0)).unwrap());
+}
