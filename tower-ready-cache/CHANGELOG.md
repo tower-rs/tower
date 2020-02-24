ba1fdd755b986ba341e80b008030920c677aef3d
Jon Gjengset <jon@thesquareplanet.com>
2020-02-24 13:09:08 -0500

    ready-cache: Prepare for 0.3.1 release
    This also fixes up the various documentation URLs, which were still
    pointing to 0.1.x.

---
+++ tower-ready-cache/CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.3.1 (February 24, 2020)
+
+- Fix spurious panic (#420).
+- Restore assertion from pre-`std::future::Future` (#418).
+- Fix documentation URLs to point to 0.3.
--- tower-ready-cache/src/lib.rs
+++ tower-ready-cache/src/lib.rs
@@ -1,6 +1,6 @@
 //! A cache of services
 
-#![doc(html_root_url = "https://docs.rs/tower-ready-cache/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/tower-ready-cache/0.3.1")]
 #![deny(missing_docs)]
 #![deny(rust_2018_idioms)]
 #![allow(elided_lifetimes_in_paths)]
