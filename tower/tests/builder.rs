70e00c6b3b00fda016ecf6b2416610cfd86ea0da
Jon Gjengset <jon@thesquareplanet.com>
2020-03-20 16:33:36 -0400

    Actually _use_ ready_and

--- tower/tests/builder.rs
+++ tower/tests/builder.rs
@@ -25,7 +25,7 @@ async fn builder_service() {
     // allow a request through
     handle.allow(1);
 
-    let fut = client.ready().await.unwrap().call("hello");
+    let fut = client.ready_and().await.unwrap().call("hello");
     assert_request_eq!(handle, "hello").send_response("world");
     assert_eq!(fut.await.unwrap(), "world");
 }
