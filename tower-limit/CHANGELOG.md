8896311cba43846aa943252cc7e5a875065691a4
Lucio Franco <luciofranco14@gmail.com>
2020-04-15 14:51:23 -0400

    limit: Prepare `0.1.2` release
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- tower-limit/CHANGELOG.md
+++ tower-limit/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.2 (April 15, 2020)
+
+- Fixed state not getting reset correctly.
+
 # 0.1.1 (October 11, 2019)
 
 - Added `tracing` events for when requests are limited
--- tower-limit/src/lib.rs
+++ tower-limit/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-limit/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tower-limit/0.1.2")]
 #![cfg_attr(test, deny(warnings))]
 #![deny(missing_debug_implementations, missing_docs, rust_2018_idioms)]
 #![allow(elided_lifetimes_in_paths)]
