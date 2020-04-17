5947e2e14549b0ba64c9445c2f1d84b7db6b07f1
Jon Gjengset <jon@thesquareplanet.com>
2020-04-17 16:03:15 -0400

    Some more spring clean fixes. (#442)
    * Add doc feature annotations
    
    * Modules should be published or removed

--- tower/src/lib.rs
+++ tower/src/lib.rs
@@ -6,6 +6,7 @@
     unreachable_pub
 )]
 #![allow(elided_lifetimes_in_paths)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! `fn(Request) -> Future<Response>`
 //!
@@ -13,44 +14,59 @@
 //! robust networking clients and servers.
 
 #[cfg(feature = "balance")]
+#[cfg_attr(docsrs, doc(cfg(feature = "balance")))]
 pub mod balance;
 #[cfg(feature = "buffer")]
+#[cfg_attr(docsrs, doc(cfg(feature = "buffer")))]
 pub mod buffer;
 #[cfg(feature = "discover")]
+#[cfg_attr(docsrs, doc(cfg(feature = "discover")))]
 pub mod discover;
 #[cfg(feature = "filter")]
-#[doc(hidden)] // not yet released
+#[cfg_attr(docsrs, doc(cfg(feature = "filter")))]
 pub mod filter;
 #[cfg(feature = "hedge")]
-#[doc(hidden)] // not yet released
+#[cfg_attr(docsrs, doc(cfg(feature = "hedge")))]
 pub mod hedge;
 #[cfg(feature = "limit")]
+#[cfg_attr(docsrs, doc(cfg(feature = "limit")))]
 pub mod limit;
 #[cfg(feature = "load")]
+#[cfg_attr(docsrs, doc(cfg(feature = "load")))]
 pub mod load;
 #[cfg(feature = "load-shed")]
+#[cfg_attr(docsrs, doc(cfg(feature = "load-shed")))]
 pub mod load_shed;
 #[cfg(feature = "make")]
+#[cfg_attr(docsrs, doc(cfg(feature = "make")))]
 pub mod make;
 #[cfg(feature = "ready-cache")]
+#[cfg_attr(docsrs, doc(cfg(feature = "ready-cache")))]
 pub mod ready_cache;
 #[cfg(feature = "reconnect")]
+#[cfg_attr(docsrs, doc(cfg(feature = "reconnect")))]
 pub mod reconnect;
 #[cfg(feature = "retry")]
+#[cfg_attr(docsrs, doc(cfg(feature = "retry")))]
 pub mod retry;
 #[cfg(feature = "spawn-ready")]
+#[cfg_attr(docsrs, doc(cfg(feature = "spawn-ready")))]
 pub mod spawn_ready;
 #[cfg(feature = "steer")]
+#[cfg_attr(docsrs, doc(cfg(feature = "steer")))]
 pub mod steer;
 #[cfg(feature = "timeout")]
+#[cfg_attr(docsrs, doc(cfg(feature = "timeout")))]
 pub mod timeout;
 #[cfg(feature = "util")]
+#[cfg_attr(docsrs, doc(cfg(feature = "util")))]
 pub mod util;
 
 pub mod builder;
 pub mod layer;
 
 #[cfg(feature = "util")]
+#[cfg_attr(docsrs, doc(cfg(feature = "util")))]
 #[doc(inline)]
 pub use self::util::{service_fn, ServiceExt};
 
