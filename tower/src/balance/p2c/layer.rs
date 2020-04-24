c1af02aad9e7a491a00ce567181a2493ab69da20
Jon Gjengset <jon@thesquareplanet.com>
2020-04-24 13:15:25 -0400

    Mention Target

--- tower/src/balance/p2c/layer.rs
+++ tower/src/balance/p2c/layer.rs
@@ -8,10 +8,10 @@ use tower_layer::Layer;
 ///
 /// This construction may seem a little odd at first glance. This is not a layer that takes
 /// requests and produces responses in the traditional sense. Instead, it is more like
-/// [`MakeService`](tower::make_service::MakeService) in that it takes service _descriptors_ and
-/// produces _services_. Since [`Balance`] spreads requests across a _set_ of services, the inner
-/// service should produce a [`Discover`], not just a single [`Service`], given a service
-/// descriptor.
+/// [`MakeService`](tower::make_service::MakeService) in that it takes service _descriptors_ (see
+/// `Target` on `MakeService`) and produces _services_. Since [`Balance`] spreads requests across a
+/// _set_ of services, the inner service should produce a [`Discover`], not just a single
+/// [`Service`], given a service descriptor.
 ///
 /// See the [module-level documentation](..) for details on load balancing.
 #[derive(Clone)]
