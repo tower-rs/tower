70d79081b862ad0ea0f99420c25202e04182baa8
Eliza Weisman <eliza@buoyant.io>
2020-12-23 10:39:06 -0800

    fixup tests expecting to use mpscs as streams
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tower/tests/support.rs
+++ tower/tests/support.rs
@@ -1,5 +1,10 @@
 #![allow(dead_code)]
 
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio::sync::mpsc;
+use tokio_stream::Stream;
+
 pub(crate) fn trace_init() -> tracing::subscriber::DefaultGuard {
     let subscriber = tracing_subscriber::fmt()
         .with_test_writer()
@@ -7,4 +12,24 @@ pub(crate) fn trace_init() -> tracing::subscriber::DefaultGuard {
         .with_thread_names(true)
         .finish();
     tracing::subscriber::set_default(subscriber)
+}
+
+#[pin_project::pin_project]
+#[derive(Clone, Debug)]
+pub struct IntoStream<S>(#[pin] pub S);
+
+impl<I> Stream for IntoStream<mpsc::Receiver<I>> {
+    type Item = I;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().0.poll_recv(cx)
+    }
+}
+
+impl<I> Stream for IntoStream<mpsc::UnboundedReceiver<I>> {
+    type Item = I;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().0.poll_recv(cx)
+    }
 }
--- tower/tests/util/call_all.rs
+++ tower/tests/util/call_all.rs
@@ -9,6 +9,7 @@ use tokio_test::{assert_pending, assert_ready, task};
 use tower::util::ServiceExt;
 use tower_service::*;
 use tower_test::{assert_request_eq, mock};
+use super::support;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
@@ -39,7 +40,7 @@ impl Service<&'static str> for Srv {
 
 #[test]
 fn ordered() {
-    let _t = super::support::trace_init();
+    let _t = support::trace_init();
 
     let mut mock = task::spawn(());
 
@@ -50,7 +51,7 @@ fn ordered() {
         admit: admit.clone(),
     };
     let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
-    let ca = srv.call_all(rx);
+    let ca = srv.call_all(support::IntoStream(rx));
     pin_mut!(ca);
 
     assert_pending!(mock.enter(|cx, _| ca.as_mut().poll_next(cx)));
@@ -112,7 +113,7 @@ fn ordered() {
 
 #[tokio::test(flavor = "current_thread")]
 async fn unordered() {
-    let _t = super::support::trace_init();
+    let _t = support::trace_init();
 
     let (mock, handle) = mock::pair::<_, &'static str>();
     pin_mut!(handle);
