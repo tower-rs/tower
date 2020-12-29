07e53066a78dcbf8c16686af2a0775fcbb375d16
David Pedersen <david.pdrsn@gmail.com>
2020-12-29 12:33:46 +0100

    Revert "Make `LayerFn` type private"
    This reverts commit d86e41e375d6551d79603bd84cf2e63cce80ce0b.

--- tower-layer/src/layer_fn.rs
+++ tower-layer/src/layer_fn.rs
@@ -1,23 +1,21 @@
 use super::Layer;
 
-/// Returns a new `impl Layer` with the given closure.
-pub fn layer_fn<F, S, T>(f: F) -> impl Layer<S, Service = T> + Clone
-where
-    F: Fn(S) -> T + Clone,
-{
+/// Returns a new `LayerFn` with the given closure.
+pub fn layer_fn<T>(f: T) -> LayerFn<T> {
     LayerFn { f }
 }
 
+/// A `Layer` implemented by a closure.
 #[derive(Clone, Copy, Debug)]
-struct LayerFn<F> {
+pub struct LayerFn<F> {
     f: F,
 }
 
-impl<F, S, T> Layer<S> for LayerFn<F>
+impl<F, S, Out> Layer<S> for LayerFn<F>
 where
-    F: Fn(S) -> T,
+    F: Fn(S) -> Out,
 {
-    type Service = T;
+    type Service = Out;
 
     fn layer(&self, inner: S) -> Self::Service {
         (self.f)(inner)
