7c7ce54a4d174f4e087fc25a2604916d2d0fb4c1
David Pedersen <david.pdrsn@gmail.com>
2020-12-30 00:33:20 +0100

    Update tower/src/make/mod.rs
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tower/src/make/mod.rs
+++ tower/src/make/mod.rs
@@ -2,8 +2,7 @@
 
 mod make_connection;
 
-pub mod make_service;
+mod make_service;
 
 pub use self::make_connection::MakeConnection;
-#[doc(inline)]
-pub use self::make_service::MakeService;
+pub use self::make_service::{MakeService, AsService, IntoService};
