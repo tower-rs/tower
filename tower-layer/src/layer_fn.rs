6d96085817d122be8a1ee56c984de6601b6f0b4f
David Pedersen <david.pdrsn@gmail.com>
2020-12-29 12:44:38 +0100

    Give `LayerFn` a more useful `Debug` implementation

--- tower-layer/src/layer_fn.rs
+++ tower-layer/src/layer_fn.rs
@@ -1,4 +1,5 @@
 use super::Layer;
+use std::fmt;
 
 /// Returns a new `LayerFn` with the given closure.
 pub fn layer_fn<T>(f: T) -> LayerFn<T> {
@@ -6,7 +7,7 @@ pub fn layer_fn<T>(f: T) -> LayerFn<T> {
 }
 
 /// A `Layer` implemented by a closure.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub struct LayerFn<F> {
     f: F,
 }
@@ -19,5 +20,33 @@ where
 
     fn layer(&self, inner: S) -> Self::Service {
         (self.f)(inner)
+    }
+}
+
+impl<F> fmt::Debug for LayerFn<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("LayerFn")
+            .field("f", &format_args!("<{}>", std::any::type_name::<F>()))
+            .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(dead_code)]
+    #[test]
+    fn layer_fn_has_useful_debug_impl() {
+        struct WrappedService<S> {
+            inner: S,
+        }
+        let layer = layer_fn(|svc| WrappedService { inner: svc });
+        let _svc = layer.layer("foo");
+
+        assert_eq!(
+            "LayerFn { f: <tower_layer::layer_fn::tests::layer_fn_has_useful_debug_impl::{{closure}}> }".to_string(),
+            format!("{:?}", layer),
+        );
     }
 }
