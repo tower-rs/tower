f2cf5cc4497d7eedfddc9423d67bd5785b7c1d5f
David Pedersen <david.pdrsn@gmail.com>
2021-01-07 00:28:09 +0100

    Update tower/src/steer/mod.rs
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- tower/src/steer/mod.rs
+++ tower/src/steer/mod.rs
@@ -5,7 +5,7 @@
 //! `Steer` can for example be used to create a router akin to what you might find in web
 //! frameworks.
 //!
-//! Here `GET /` will be sent to the `root` service all other requests go to `not_found`.
+//! Here, `GET /` will be sent to the `root` service, while all other requests go to `not_found`.
 //!
 //! ```rust
 //! # use std::task::{Context, Poll};
