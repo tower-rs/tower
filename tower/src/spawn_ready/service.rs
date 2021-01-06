ba3679366d278971e75cf5376c251e21a898ba0d
Eliza Weisman <eliza@buoyant.io>
2021-01-06 12:38:23 -0800

    whoopsie x2
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tower/src/spawn_ready/service.rs
+++ tower/src/spawn_ready/service.rs
@@ -66,7 +66,9 @@ where
 
     fn call(&mut self, request: Request) -> Self::Future {
         match self.inner {
-            Inner::Service(Some(ref mut svc)) => svc.call(request).map_err(Into::into),
+            Inner::Service(Some(ref mut svc)) => {
+                ResponseFuture(svc.call(request).map_err(Into::into))
+            }
             _ => unreachable!("poll_ready must be called"),
         }
     }
