44ad621c56e437d690bcd193313b81acc168895b
Eliza Weisman <eliza@buoyant.io>
2021-01-07 15:05:45 -0800

    layer: prepare to release v0.3.1 (#513)
    * layer: prepare to release v0.3.1
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    
    * whitespace

--- tower-layer/src/lib.rs
+++ tower-layer/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-layer/0.3.0-alpha.2")]
+#![doc(html_root_url = "https://docs.rs/tower-layer/0.3.1")]
 #![warn(
     missing_debug_implementations,
     missing_docs,
