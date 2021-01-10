0a1f5c7219aaafda607c995797661441e78b9690
David Pedersen <david.pdrsn@gmail.com>
2021-01-10 20:50:39 +0100

    Re-export `layer_fn` and `LayerFn` from tower

--- tower/src/layer.rs
+++ tower/src/layer.rs
@@ -1,6 +1,6 @@
 //! A collection of `Layer` based tower services
 
-pub use tower_layer::Layer;
+pub use tower_layer::{layer_fn, Layer, LayerFn};
 
 /// `util` exports an Identity Layer and Chain, a mechanism for chaining them.
 pub mod util {
