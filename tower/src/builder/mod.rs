3a17d3673d0b4839f564f590487663e02f7da5f8
David Pedersen <david.pdrsn@gmail.com>
2021-02-13 19:11:28 +0100

    builder: Add `ServiceBuilder::map_future`
    I forgot at add this one in #542. Think its nice to have for
    consistency.

--- tower/src/builder/mod.rs
+++ tower/src/builder/mod.rs
@@ -405,6 +405,18 @@ impl<L> ServiceBuilder<L> {
         self.layer(crate::util::MapErrLayer::new(f))
     }
 
+    /// Composes a function that transforms futures produced by the service.
+    ///
+    /// This wraps the inner service with an instance of the [`MapFutureLayer`] middleware.
+    ///
+    /// See the documentation for the [`map_future`] combinator for details.
+    ///
+    /// [`MapFutureLayer`]: crate::util::MapFutureLayer
+    /// [`map_future`]: crate::util::ServiceExt::map_future
+    pub fn map_future<F>(self, f: F) -> ServiceBuilder<Stack<crate::util::MapFutureLayer<F>, L>> {
+        self.layer(crate::util::MapFutureLayer::new(f))
+    }
+
     /// Apply a function after the service, regardless of whether the future
     /// succeeds or fails.
     ///
