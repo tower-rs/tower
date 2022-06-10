8b522920f8e1ece656d5ed0d37230efc47542906
Matt Klein <mattklein123@gmail.com>
2022-06-10 11:52:39 -0600

    service: clarify docs around shared resource consumption in poll_ready() (#662)
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- tower-service/src/lib.rs
+++ tower-service/src/lib.rs
@@ -331,6 +331,12 @@ pub trait Service<Request> {
     /// Once `poll_ready` returns `Poll::Ready(Ok(()))`, a request may be dispatched to the
     /// service using `call`. Until a request is dispatched, repeated calls to
     /// `poll_ready` must return either `Poll::Ready(Ok(()))` or `Poll::Ready(Err(_))`.
+    ///
+    /// Note that `poll_ready` may reserve shared resources that are consumed in a subsequent
+    /// invocation of `call`. Thus, it is critical for implementations to not assume that `call`
+    /// will always be invoked and to ensure that such resources are released if the service is
+    /// dropped before `call` is invoked or the future returned by `call` is dropped before it
+    /// is polled.
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>>;
 
     /// Process the request and return the response asynchronously.
