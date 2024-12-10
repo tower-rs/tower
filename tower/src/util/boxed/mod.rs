af7a83bc9f8e04216aba11bbcf063e2ee27519b7
JESS IZEN <jlizen@amazon.com>
2024-12-10 19:20:42 +0000

    run rustfmt

--- tower/src/util/boxed/mod.rs
+++ tower/src/util/boxed/mod.rs
@@ -6,5 +6,6 @@ mod unsync;
 
 #[allow(unreachable_pub)] // https://github.com/rust-lang/rust/issues/57411
 pub use self::{
-    layer::BoxLayer, layer_clone::BoxCloneServiceLayer, layer_clone_sync::BoxCloneSyncServiceLayer, sync::BoxService, unsync::UnsyncBoxService,
+    layer::BoxLayer, layer_clone::BoxCloneServiceLayer, layer_clone_sync::BoxCloneSyncServiceLayer,
+    sync::BoxService, unsync::UnsyncBoxService,
 };
