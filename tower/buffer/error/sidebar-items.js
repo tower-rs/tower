3f7ca4a7b96ecf4d7bf3110672d7294aa1785c5c
tottoto <tottoto@users.noreply.github.com>
2026-01-28 05:14:59 +0000

    Deploy to GitHub pages

---
+++ tower/buffer/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Closed","ServiceError"]};
---
+++ trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["tower",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"tower/load/peak_ewma/struct.Cost.html\" title=\"struct tower::load::peak_ewma::Cost\">Cost</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"tower/load/pending_requests/struct.Count.html\" title=\"struct tower::load::pending_requests::Count\">Count</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[588]}
