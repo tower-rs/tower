42a2c9a1ce75d56ec9c1ac6498a79d1b66befa06
Alpcan Ornek <alpcan0708@gmail.com>
2026-05-21 17:01:43 +0200

    docs(steer): add explicit # Panics section to constructor

--- tower/src/steer/mod.rs
+++ tower/src/steer/mod.rs
@@ -113,6 +113,10 @@ pub struct Steer<S, F, Req> {
 impl<S, F, Req> Steer<S, F, Req> {
     /// Make a new [`Steer`] with a list of [`Service`]'s and a [`Picker`].
     ///
+    /// # Panics
+    ///
+    /// Panics if the `services` collection is empty
+    ///
     /// Note: the order of the [`Service`]'s is significant for [`Picker::pick`]'s return value.
     pub fn new(services: impl IntoIterator<Item = S>, router: F) -> Self {
         let services: Vec<_> = services.into_iter().collect();
