b2494f34533d484682aeac449ceced160166dc9e
Mira Sato <275437409+oab24413gmai@users.noreply.github.com>
2026-05-21 16:53:36 +0000

    chore: improve tower maintenance path

--- tower/src/timeout/mod.rs
+++ tower/src/timeout/mod.rs
@@ -68,3 +68,72 @@ where
         ResponseFuture::new(response, sleep)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        convert::Infallible,
+        future::Future,
+        pin::Pin,
+        task::{Context, Poll},
+        time::Duration,
+    };
+    use tokio::time::sleep;
+    use tower_service::Service;
+
+    struct SlowService(Duration);
+
+    impl Service<()> for SlowService {
+        type Response = ();
+        type Error = Infallible;
+        type Future = Pin<Box<dyn Future<Output = Result<(), Infallible>> + Send>>;
+
+        fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, _req: ()) -> Self::Future {
+            let delay = self.0;
+            Box::pin(async move {
+                sleep(delay).await;
+                Ok(())
+            })
+        }
+    }
+
+    struct FastService;
+
+    impl Service<()> for FastService {
+        type Response = &'static str;
+        type Error = Infallible;
+        type Future = Pin<Box<dyn Future<Output = Result<&'static str, Infallible>> + Send>>;
+
+        fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, _req: ()) -> Self::Future {
+            Box::pin(async move { Ok("ok") })
+        }
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn elapsed_error_when_timeout_exceeded() {
+        let mut svc = Timeout::new(SlowService(Duration::from_secs(10)), Duration::from_secs(1));
+
+        let res = svc.call(()).await;
+        assert!(res.is_err());
+
+        let err = res.unwrap_err();
+        assert!(err.downcast_ref::<error::Elapsed>().is_some());
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn response_passes_through_when_under_timeout() {
+        let mut svc = Timeout::new(FastService, Duration::from_secs(1));
+
+        let res = svc.call(()).await;
+        assert_eq!(res.unwrap(), "ok");
+    }
+}
